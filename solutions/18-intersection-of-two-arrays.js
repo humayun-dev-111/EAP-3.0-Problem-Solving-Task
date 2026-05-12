3646085e8aaa21acabfd2d1d301b099944cad653
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2026-05-12 21:37:21 +0600

    intersection of two arrays problem solved

---
+++ solutions/18-intersection-of-two-arrays.js
@@ -0,0 +1,15 @@
+// Find the intersection of two arrays
+// Write a function that returns the common elements between two arrays.
+function findIntersection(arr1, arr2) {
+  let result = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !result.includes(arr1[i])) {
+      result.push(arr1[i]);
+    }
+  }
+
+  return result;
+}
+
+console.log(findIntersection([1, 2, 3, 4], [3, 4, 5, 6]));
