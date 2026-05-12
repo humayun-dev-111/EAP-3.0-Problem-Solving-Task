243302f0d6e61c9e9e682d1cf818d384ed228d35
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2026-05-12 21:33:10 +0600

    marge two sorted arrays problem solved

---
+++ solutions/13-merge-two-sorted-arrays.js
@@ -0,0 +1,31 @@
+// Merge two sorted arrays
+// Write a function to merge two sorted arrays into one sorted array without using sort().
+function mergeSortedArrays(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6]));
